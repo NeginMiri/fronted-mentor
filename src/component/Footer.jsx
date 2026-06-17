ea18e78e6c80129c543fd75ab611bd5092ccdc49
ahourasoheily <Ahourasoheily2007@gamil.com>
2026-06-17 15:30:14 +0330

    final-vrsion

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -7,16 +7,30 @@ const Footer = () => {
         <h1 className=" flex text text-[22px] text-tx-400 mr-1.5">
           Project page, and code written by
         </h1>
-        <a className="flex text text-[22px] text-white cursor-pointer mr-1">
+        <a
+          href="https://github.com/NeginMiri"
+          target="_blank"
+          className="flex text text-[22px] text-white cursor-pointer mr-1"
+        >
           Negin
         </a>
-        <a className=" text text-[22px] text-white cursor-pointer ">Ahoura</a>
+        <a
+          href="https://github.com/Ahourasoheily"
+          target="_blank"
+          className=" text text-[22px] text-white cursor-pointer "
+        >
+          Ahoura
+        </a>
       </div>
       <div className="w-full flex justify-center">
         <h1 className="flex text text-[22px] text-tx-400 mr-1.5">
           Designed by
         </h1>
-        <a className="flex text text-[22px] text-white mr-1.5">
+        <a
+          href="https://www.frontendmentor.io/"
+          target="_blank"
+          className="flex text text-[22px] text-white mr-1.5"
+        >
           Fronted Mentor
         </a>
       </div>
